dcfdb4d87cf7b5fa7ae88640b334c24eece1628c
Julia <vondel@mail.ru>
2023-01-18 11:27:55 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+            int ticket = 10_000;
+            int mile = 20;
+            int bonus = ticket / mile;
+
+            System.out.println("Количество бонусных миль:" + bonus);
+
+                    }
+
+
+
+
+}
